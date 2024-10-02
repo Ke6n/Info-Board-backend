ed5f569d1fa6435d1f580eb323b7c16ace6b7fef
Ke6n <lk8619@163.com>
2024-10-02 21:28:29 +0200

    add login function

--- src/main/java/com/info_board/controller/UserController.java
+++ src/main/java/com/info_board/controller/UserController.java
@@ -3,6 +3,7 @@ package com.info_board.controller;
 import com.info_board.pojo.Result;
 import com.info_board.pojo.User;
 import com.info_board.service.UserService;
+import com.info_board.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -18,16 +19,27 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
+    public Result<String> register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
         // user query
         User user = userService.findByUserName(username);
         if (user == null){
+            // create user
             userService.register(username, password);
             return Result.success();
         }else {
             return Result.error("The username is already in use!");
         }
-        // create user
+    }
 
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
+        // user query
+        User loginUser = userService.findByUserName(username);
+        // password verify
+        if(loginUser != null && Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            return Result.success();
+        }
+        // Username or password is wrong
+        return Result.error("Please enter the correct username or password!");
     }
 }
